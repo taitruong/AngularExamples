6e5e41b2c36e06bbf6b605006d2e01c206819cbe
taitruong <me@mr-t.org>
2019-03-04 15:32:01 +0100

    new Angular project: > rm AngularExamples/.gitignore > rm AngularExamples/README.md > ng n AngularExamples -s --style scss --prefix nge --no-routing

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nge-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'AngularExamples';
+}
