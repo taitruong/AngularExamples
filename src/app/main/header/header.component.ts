f1d1d37471f04239eaf3e600cf3edd92300c8ded
taitruong <me@mr-t.org>
2019-03-04 18:24:05 +0100

    create new component: > ng g c main/header

---
+++ src/app/main/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nge-header',
+  templateUrl: './header.component.html',
+  styles: []
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
