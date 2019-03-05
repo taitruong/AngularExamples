db461710824e7f1a8a46663fd0231d7776f4ba36
taitruong <me@mr-t.org>
2019-03-05 21:00:01 +0100

    create new component: ng g c main/home

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, HomeComponent],
   imports: [
     CommonModule
   ],
