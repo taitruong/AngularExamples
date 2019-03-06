b8458be691020eadcd0d0eded9283a3697827cdb
taitruong <me@mr-t.org>
2019-03-06 18:58:26 +0100

    clean up: move main logic from app to home component, routing needed only for installation component, export only home component in main module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,8 @@
-import { HomeComponent } from './main/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InstallationComponent } from './getting-started/installation/installation.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    component: HomeComponent
-  },
   {
     path: 'installation',
     component: InstallationComponent
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -13,7 +13,7 @@ import { RouterModule } from '@angular/router';
     GettingStartedModule
   ],
   exports: [
-    HeaderComponent
+    HomeComponent
   ]
 })
 export class MainModule { }
