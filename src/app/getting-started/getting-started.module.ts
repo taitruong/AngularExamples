056b373ab1367e655982e6bc84afdf0e6d6a8be3
taitruong <me@mr-t.org>
2019-03-06 09:38:44 +0100

    new installation component

--- src/app/getting-started/getting-started.module.ts
+++ src/app/getting-started/getting-started.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InstallationComponent } from './installation/installation.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [InstallationComponent],
   imports: [
     CommonModule
   ]
---
+++ src/app/getting-started/installation/installation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nge-installation',
+  templateUrl: './installation.component.html',
+  styles: []
+})
+export class InstallationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
